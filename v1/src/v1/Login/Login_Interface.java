13c768a5eccde20130c4c2145df712b9907bf196
Sanjog Wanem <37484722+Swanem24@users.noreply.github.com>
2019-03-13 20:58:05 +0000

    login

--- v1/src/v1/Login/Login_Interface.java
+++ v1/src/v1/Login/Login_Interface.java
@@ -5,6 +5,10 @@
  */
 package v1.Login;
 
+import java.awt.Color;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author up818044
@@ -16,6 +20,9 @@ public class Login_Interface extends javax.swing.JFrame {
      */
     public Login_Interface() {
         initComponents();
+        
+        //Sets the form in the centre
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -32,9 +39,13 @@ public class Login_Interface extends javax.swing.JFrame {
         jPanel5 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
         jPanel7 = new javax.swing.JPanel();
+        jTextField2 = new javax.swing.JTextField();
         jPanel3 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
         jPanel8 = new javax.swing.JPanel();
+        jTextField_Email = new javax.swing.JTextField();
+        jPasswordField = new javax.swing.JPasswordField();
+        jButton_Login = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -96,21 +107,81 @@ public class Login_Interface extends javax.swing.JFrame {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
+        jTextField2.setText("jTextField1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel3.setBackground(new java.awt.Color(0, 0, 255));
 
         jPanel4.setBackground(new java.awt.Color(51, 51, 51));
 
+        jTextField_Email.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jTextField_Email.setForeground(new java.awt.Color(153, 153, 153));
+        jTextField_Email.setText("email");
+        jTextField_Email.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jTextField_Email.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jTextField_EmailFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jTextField_EmailFocusLost(evt);
+            }
+        });
+        jTextField_Email.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField_EmailActionPerformed(evt);
+            }
+        });
+
+        jPasswordField.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jPasswordField.setForeground(new java.awt.Color(153, 153, 153));
+        jPasswordField.setText("Password");
+        jPasswordField.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPasswordField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                jPasswordFieldFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jPasswordFieldFocusLost(evt);
+            }
+        });
+        jPasswordField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordFieldActionPerformed(evt);
+            }
+        });
+
+        jButton_Login.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jButton_Login.setText("Login");
+        jButton_Login.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jButton_Login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_LoginActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel8Layout = new javax.swing.GroupLayout(jPanel8);
         jPanel8.setLayout(jPanel8Layout);
         jPanel8Layout.setHorizontalGroup(
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel8Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton_Login, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField_Email, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(97, 97, 97))
         );
         jPanel8Layout.setVerticalGroup(
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 244, Short.MAX_VALUE)
+            .addGroup(jPanel8Layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jTextField_Email, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jButton_Login, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
@@ -123,7 +194,7 @@ public class Login_Interface extends javax.swing.JFrame {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addContainerGap(124, Short.MAX_VALUE)
+                .addContainerGap(91, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(112, 112, 112))
         );
@@ -131,7 +202,7 @@ public class Login_Interface extends javax.swing.JFrame {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 64, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addComponent(jPanel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -166,6 +237,76 @@ public class Login_Interface extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField_EmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField_EmailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField_EmailActionPerformed
+
+    private void jPasswordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordFieldActionPerformed
+
+    private void jTextField_EmailFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField_EmailFocusGained
+        //Clear the textfield
+        if(jTextField_Email.getText().trim().toLowerCase().equals("email"))
+        {
+            jTextField_Email.setText("");
+            jTextField_Email.setForeground(Color.BLACK);
+        }
+    }//GEN-LAST:event_jTextField_EmailFocusGained
+
+    private void jTextField_EmailFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField_EmailFocusLost
+        // if the text field is equal to email or empty
+        if(jTextField_Email.getText().trim().equals("") || jTextField_Email.getText().trim().toLowerCase().equals("email"))
+        {
+            jTextField_Email.setText("email");
+            jTextField_Email.setForeground(new Color(153,153,153));
+        }
+    }//GEN-LAST:event_jTextField_EmailFocusLost
+
+    private void jPasswordFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordFieldFocusGained
+        //Clear the textfield
+        
+        //Get Password Text
+        String pass = String.valueOf(jPasswordField.getPassword());
+        
+        if(pass.trim().toLowerCase().equals("password"))
+        {
+            jPasswordField.setText("");
+            jPasswordField.setForeground(Color.BLACK);
+        }
+    }//GEN-LAST:event_jPasswordFieldFocusGained
+
+    private void jPasswordFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordFieldFocusLost
+        // if the text field is equal to password or empty
+        
+        //Get Password Text
+        String pass = String.valueOf(jPasswordField.getPassword());
+        
+        if(pass.trim().equals("") || jPasswordField.getText().trim().toLowerCase().equals("password"))
+        {
+            jPasswordField.setText("password");
+            jPasswordField.setForeground(new Color(153,153,153));
+        }
+    }//GEN-LAST:event_jPasswordFieldFocusLost
+
+    private void jButton_LoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_LoginActionPerformed
+        
+        PreparedStatement st;
+        ResultSet rs;
+        
+        // get the username and password
+        String username = jTextField_Email.getText();
+        String password = String.valueOf(jPasswordField.getPassword());
+        
+        //create a select query to check if the username and the password exist in the database
+        String query = "SELECT * FROM 'user' WHERE 'emal' = ? AND 'password' = ?";
+        
+        st = DBConnect.getConnection().prepareStatement(query);
+        
+        
+        
+    }//GEN-LAST:event_jButton_LoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -203,6 +344,7 @@ public class Login_Interface extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Login;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -212,5 +354,8 @@ public class Login_Interface extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
     private javax.swing.JPanel jPanel8;
+    private javax.swing.JPasswordField jPasswordField;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField_Email;
     // End of variables declaration//GEN-END:variables
 }
---
+++ v1/src/v1/Login/loginConnect.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package v1.Login;
+
+/**
+ *
+ * @author thewa
+ */
+public class loginConnect {
+    
+}
