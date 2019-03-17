8681b42efdde492c50e0394a856b9171e046a066
Konstantin192 <konsstantin.georgiev@gmail.com>
2019-03-17 17:36:32 +0000

    Touched up code
    Removed obsolete code, added comments and formatted code in certain places.

--- v1/src/v1/User.java
+++ v1/src/v1/User.java
@@ -29,30 +29,36 @@ public class User {
                  Integer u_UserID, String u_UserName, String u_Password, 
                  Integer u_NumTasksDone, ArrayList<String> u_BestSkills) {
         
-        firstName = u_FName;
-        lastName = u_LName;
-        email = u_Email;
-        userID = u_UserID;
-        userName = u_UserName;
-        password = u_Password;
-        numTasksDone = u_NumTasksDone;
-        bestSkills = u_BestSkills;
+        this.firstName = u_FName;
+        this.lastName = u_LName;
+        this.email = u_Email;
+        this.userID = u_UserID;
+        this.userName = u_UserName;
+        this.password = u_Password;
+        this.numTasksDone = u_NumTasksDone;
+        this.bestSkills = u_BestSkills;
         
         
        //insert into query
     }
         
-       public static void insert (String uFName, String uSName, String uEmail, String uPassword, String uUsername, Integer numTasksDone) {
+       public static void insert (String uFName, String uSName, String uEmail, 
+                                  String uPassword, String uUsername, 
+                                  Integer numTasksDone) {
 //           
         String host = "jdbc:mysql://den1.mysql3.gear.host:3306/teammanagerdb";
         String uName = "teammanagerdb";
         String uPass = "Bc85NMS--V6h";
                         
-        
-        String insert = "INSERT INTO user(User_Forename, User_Surname, User_Email, User_Password, User_Username, User_NumTasksDone) VALUES(?, ?, ?, ?, ?, ?)";
+        // The question marks in the values brackets are SQL's equivalent to 
+        // variables.
+        String insert = "INSERT INTO user(User_Forename, User_Surname, User_Email, User_Password, User_Username, User_NumTasksDone) "
+                        + "VALUES(?, ?, ?, ?, ?, ?)";
 
         try (Connection con = DriverManager.getConnection(host, uName, uPass);) {
             
+            // A prepared statement is created and values are assigned to the 
+            // question marks in the values brackets.
             PreparedStatement pst = con.prepareStatement(insert);
             pst.setString(1, uFName);
             pst.setString(2, uSName);
@@ -64,34 +70,15 @@ public class User {
             pst.executeUpdate();
             pst.close();
             
-            // create the java statement
-            //Statement stat = con.createStatement();
-            // execute the query, and get a java resultset
-            //int rs;
-            //rs = stat.executeUpdate(insert);
-            
-//            String firstName = ("roger");
-//            String lastName = ("rabbit");
-//            String email = ("carrots@nmail.com");
-            
-                
-//            pst.setString(1,"roger");
-//            pst.setString(2,"rabbit");
-//            pst.setString(3,"carrots@nmail.com");
-//            pst.executeUpdate();
-            
             System.out.println("A new member has been inserted");
 
         } catch (SQLException ex) {
 
             Logger lgr = Logger.getLogger(DBConnect.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
-//needs stat.close();
 
         }
-        }
-       
-       
+       }
     }
     
   
